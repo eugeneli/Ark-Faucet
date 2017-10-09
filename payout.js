479c5c99224f06f85af29a88a300ceef31afc2be
Eugene Li <hello@itseugene.li>
2017-10-09 04:29:57 -0400

    payout code

--- payout.js
+++ payout.js
@@ -1,4 +1,4 @@
-//var repo = require("./api/repos/faucetRepo");
+var repo = require("./api/repos/faucetRepo");
 var arkApi = require("ark-api");
 var moment = require("moment");
 var BigNumber = require("bignumber.js");
@@ -9,6 +9,46 @@ var doPayout = (threshold, fee, passphrase, secondPassphrase) => {
     console.log("DateTime: " + moment().toISOString());
     console.log("Getting pending balances...");
 
+    repo.getOverthresholdBalances(threshold).then((balances) => {
+        if(balances.length == 0)
+            return;
+
+        var options = { secondPassphrase: secondPassphrase };
+
+        var addrs = balances.map((bal) => bal.address);
+        var txs = balances.map((bal) => {
+            var payout = new BigNumber(bal.pending).times(100000000); //convert to arktoshis
+            var txFee = new BigNumber(fee).times(100000000);
+            payout = payout.minus(txFee);
+
+            var tx = arkApi.createTransaction(passphrase, bal.address, payout.toString(), options);
+            var logMsg = balance.wallet + " " + parseFloat(pendingBalance) + " " + tx.id;
+            console.log(logMsg);
+            
+            return tx;
+        });
+
+        repo.deleteUnpaidBalances(addrs);
+
+        var txBundles = [];
+        while(txs.length)
+            txBundles.push(txs.splice(0, 10));
+
+        console.log("Paying now...");
+        var i = 0;
+        function queuePayments()
+        {
+            var bundle = txBundles[i];
+            console.log(`Sending tx bundle ${i+1}/${txBundles.length}`);
+            arkApi.sendTransactions(bundle);
+            i++;
+            if(i < txBundles.length)
+                setTimeout(queuePayments, 5000);
+            else
+                console.log("==Payout Complete==");
+        }
+        queuePayments();
+    });
 };
 
 exports.startScheduler = (threshold, fee, cronJob, passphrase, secondPassphrase) => {
